7d23b88adeb6488934e8e4dca2ed1388b414291d
ozihler <ozihler@gmail.com>
2019-09-14 15:29:23 +0200

    extend in memory customer repository

--- backend/src/main/java/com/zihler/library/gatewayadapters/InMemoryCustomerRepository.java
+++ backend/src/main/java/com/zihler/library/gatewayadapters/InMemoryCustomerRepository.java
@@ -2,8 +2,18 @@ package com.zihler.library.gatewayadapters;
 
 import com.zihler.library.entities.Customer;
 
+import java.util.Map;
+
 public class InMemoryCustomerRepository {
+    private final Map<String, Customer> customers;
+
+    public InMemoryCustomerRepository() {
+        customers = Map.of(
+                "AnyUser", new Customer("anyUser")
+        );
+    }
+
     public Customer findByUsername(String username) {
-        return new Customer(username);
+        return customers.getOrDefault(username, new Customer(username));
     }
 }
