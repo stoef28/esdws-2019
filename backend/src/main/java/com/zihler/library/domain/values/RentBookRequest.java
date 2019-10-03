b5389cf360128dd97bcaafe3e6167be9fb87ad05
ozihler <ozihler@gmail.com>
2019-10-03 18:09:08 +0200

    Introduce RentBookRequest

---
+++ backend/src/main/java/com/zihler/library/domain/values/RentBookRequest.java
@@ -0,0 +1,19 @@
+package com.zihler.library.domain.values;
+
+public class RentBookRequest {
+    private final int bookId;
+    private final int daysRented;
+
+    public RentBookRequest(int bookId, int daysRented) {
+        this.bookId = bookId;
+        this.daysRented = daysRented;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
