c767b369c7ac24d0badab7dafbb3e1717b9a3a0b
ozihler <ozihler@gmail.com>
2019-10-03 19:18:50 +0200

    Extract list of RentBookRequests

--- backend/src/main/java/com/zihler/library/LibraryResource.java
+++ backend/src/main/java/com/zihler/library/LibraryResource.java
@@ -58,7 +58,7 @@ public class LibraryResource {
         Customer customer = customerRepository.findByUsername(customerName);
 
         // calculate fee, frequent renter points, and document to display in front end
-        List<Rental> rentals = new ArrayList<>();
+        List<RentBookRequest> rentBookRequests = new ArrayList<>();
         for (int i = 0; i < rentBooksRequests.size(); i++) {
             final String[] rentalData = rentBooksRequests.get(i).split(" ");
 
@@ -66,7 +66,12 @@ public class LibraryResource {
             DaysRented daysRented = DaysRented.from(rentalData[1]);
 
             RentBookRequest rentBookRequest = new RentBookRequest(bookId, daysRented);
+            RentBookRequest rentBookRequest = new RentBookRequest(Integer.parseInt(rentalData[0]), Integer.parseInt(rentalData[1]));
+            rentBookRequests.add(rentBookRequest);
+        }
 
+        List<Rental> rentals = new ArrayList<>();
+        for (RentBookRequest rentBookRequest : rentBookRequests) {
             Book book = bookRepository.findById(rentBookRequest.getBookId());
             Rental rental = new Rental(book, rentBookRequest.getDaysRented());
             rentals.add(rental);
