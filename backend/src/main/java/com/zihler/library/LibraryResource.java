1d73c36c76ca801d2695582ac937f921fc595279
ozihler <ozihler@gmail.com>
2019-10-03 17:54:40 +0200

    Loop split

--- backend/src/main/java/com/zihler/library/LibraryResource.java
+++ backend/src/main/java/com/zihler/library/LibraryResource.java
@@ -56,30 +56,60 @@ public class LibraryResource {
         Customer customer = customerRepository.findByUsername(customerName);
 
         // calculate fee, frequent renter points, and document to display in front end
+        List<Rental> rentals = new ArrayList<>();
         Amount totalAmount = Amount.of(0);
         FrequentRenterPoints frequentRenterPoints = FrequentRenterPoints.of(0);
         String result = "Rental Record for " + customer.getName() + "\n";
 
         for (int i = 0; i < rentBooksRequests.size(); i++) {
             final String[] rentalData = rentBooksRequests.get(i).split(" ");
+            int bookId = Integer.parseInt(rentalData[0]);
+            int daysRented = Integer.parseInt(rentalData[1]);
 
             BookId bookId = BookId.from(rentalData[0]);
             DaysRented daysRented = DaysRented.from(rentalData[1]);
 
             Book book = bookRepository.findById(bookId);
             Rental rental = new Rental(book, daysRented);
+            rentals.add(rental);
+        }
+
+        String result = "Rental Record for " + customer.getName() + "\n";
+        result += format(rentals);
+        // add footer lines
+        result += "You owe " + getTotalAmount(rentals) + " $\n";
+        result += "You earned " + getFrequentRenterPoints(rentals) + " frequent renter points\n";
+
+        return List.of(result);
+    }
+
+    private double getTotalAmount(List<Rental> rentals) {
+        double totalAmount = 0;
+        for (Rental rental : rentals) {
+            totalAmount += rental.getAmount();
+        }
+        return totalAmount;
+    }
+
+    private int getFrequentRenterPoints(List<Rental> rentals) {
+        int frequentRenterPoints = 0;
+        for (Rental rental : rentals) {
+            frequentRenterPoints += rental.getFrequentRenterPoints();
+        }
+        return frequentRenterPoints;
+    }
+
+    private String format(List<Rental> rentals) {
+        String result="";
+        for (Rental rental : rentals) {
 
             frequentRenterPoints.plus(rental.frequentRenterPoints());
             // create figures for this rental
+            result += "\t'" + rental.getBookTitle() + "' by '" + rental.getBookAuthors() + "' for " + rental.getDaysRented() + " days: \t" + rental.getAmount() + " $\n";
             result += "\t'" + rental.bookTitle() + "' by '" + rental.bookAuthors() + "' for " + rental.daysRented() + " days: \t" + rental.amount() + " $\n";
             totalAmount.plus(rental.amount());
         }
-
-        // add footer lines
-        result += "You owe " + totalAmount + " $\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points\n";
-
-        return List.of(result);
+        return result;
     }
 
 }
