eef162ebd132806639ce5f764c362a86113b657b
ozihler <ozihler@gmail.com>
2019-10-04 15:13:20 +0200

    fix it

--- backend/src/main/java/com/zihler/library/adapters/rest/RestRentalRecordPresenter.java
+++ backend/src/main/java/com/zihler/library/adapters/rest/RestRentalRecordPresenter.java
@@ -12,11 +12,11 @@ public class RestRentalRecordPresenter {
     }
 
     public void present(RentalRecord rentalRecord) {
-        String result = "Rental Record for " + rentalRecord.getCustomerName() + "\n";
-        result += format(rentalRecord.getRentals());
+        String result = "Rental Record for " + rentalRecord.customerName() + "\n";
+        result += format(rentalRecord.rentals());
         // add footer lines
-        result += "You owe " + rentalRecord.getTotalAmount() + " $\n";
-        result += "You earned " + rentalRecord.getFrequentRenterPoints() + " frequent renter points\n";
+        result += "You owe " + rentalRecord.totalAmount() + " $\n";
+        result += "You earned " + rentalRecord.frequentRenterPoints() + " frequent renter points\n";
 
         this.restRentalRecord = List.of(result);
     }
@@ -25,7 +25,7 @@ public class RestRentalRecordPresenter {
         String result = "";
         for (Rental rental : rentals) {
             // create figures for this rental
-            result += "\t'" + rental.getBookTitle() + "' by '" + rental.getBookAuthors() + "' for " + rental.getDaysRented() + " days: \t" + rental.getAmount() + " $\n";
+            result += "\t'" + rental.bookTitle() + "' by '" + rental.bookAuthors() + "' for " + rental.daysRented() + " days: \t" + rental.amount() + " $\n";
         }
         return result;
     }
