bebe933a47c572307665a41142a1d11929efc899
ozihler <ozihler@gmail.com>
2019-10-05 18:17:23 +0200

    Revert "Use Integer as Book ID"
    This reverts commit a61a9ec0

--- backend/src/main/java/com/zihler/library/domain/entities/Book.java
+++ backend/src/main/java/com/zihler/library/domain/entities/Book.java
@@ -1,13 +1,13 @@
 package com.zihler.library.domain.entities;
 
 public class Book {
-    private final int id;
+    private final String id;
     private final String title;
     private final String authors;
     private final String readingMode;
     private final String thumbnailLink;
 
-    public Book(int id, String authors, String title, String readingMode, String thumbnailLink) {
+    public Book(String id, String title, String authors, String readingMode, String thumbnailLink) {
         this.id = id;
         this.title = title;
         this.authors = authors;
