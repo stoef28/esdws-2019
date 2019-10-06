2e2f2994632d855513fc332ad996bff33bb5aeb4
ozihler <ozihler@gmail.com>
2019-10-06 15:03:13 +0200

    move from use_cases to application.use_cases

--- backend/src/main/java/com/zihler/library/application/use_cases/rent_books/RentBooks.java
+++ backend/src/main/java/com/zihler/library/application/use_cases/rent_books/RentBooks.java
@@ -1,4 +1,4 @@
-package com.zihler.library.use_cases.rent_books;
+package com.zihler.library.application.use_cases.rent_books;
 
 import com.zihler.library.InMemoryCustomerRepository;
 import com.zihler.library.adapters.file_persistance.FileBasedBookRepository;
@@ -7,8 +7,8 @@ import com.zihler.library.domain.entities.Book;
 import com.zihler.library.domain.entities.Customer;
 import com.zihler.library.domain.values.Rental;
 import com.zihler.library.domain.values.RentalRecord;
-import com.zihler.library.use_cases.rent_books.ports.RentBookRequest;
-import com.zihler.library.use_cases.rent_books.ports.RentBooksRequest;
+import com.zihler.library.application.use_cases.rent_books.ports.RentBookRequest;
+import com.zihler.library.application.use_cases.rent_books.ports.RentBooksRequest;
 
 import java.util.ArrayList;
 import java.util.List;
